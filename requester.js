047a8b1e5e8e24f241c3494e1ea81230a3d68323
Oseid <eoseid@amazon.com>
2020-04-01 20:08:23 -0700

    refactoring requester

--- requester.js
+++ requester.js
@@ -1,43 +1,28 @@
 const http = require('http');
 
-const options = {
-	hostname : '127.0.0.1',
-	port : 3000,
-	path : '/bigboi',
-	method : 'GET',
+function localGetRequest(path, port) {
+
+	const options = {
+		hostname : '127.0.0.1',
+		path : path,
+		port : port,
+		method : 'GET',
+	}
+
+	const req = http.request(options, (res) => {
+		res.setEncoding('utf8');
+
+		res.on('data', (chunk) => {
+			console.log(chunk);
+		});
+
+		res.on('end', () => {
+			console.log('No more data in request');
+		});
+	});
+
+	req.end();
 }
 
-const req = http.request(options, (res) => {
-	res.setEncoding('utf8');
-
-	res.on('data', (chunk) => {
-		console.log(chunk);
-	});
-
-	res.on('end', () => {
-		console.log('No more data in request');
-	});
-});
-
-req.end();
-
-const options1 = {
-	hostname : '127.0.0.1',
-	port : 3001,
-	path : '/biggirl',
-	method : 'GET',
-}
-
-const req1 = http.request(options1, (res) => {
-	res.setEncoding('utf8');
-
-	res.on('data', (chunk) => {
-		console.log(chunk);
-	});
-
-	res.on('end', () => {
-		console.log('No more data in request');
-	});
-});
-
-req1.end();
+localGetRequest('/bigboi', 3000);
+localGetRequest('/biggirl', 3001);
